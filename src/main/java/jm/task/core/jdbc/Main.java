d9262a9c83846de84e5609873a20de927abcbaac
Max Ivanov <rellepro@gmail.com>
2021-01-18 00:25:42 +0300

    Test create table

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,44 @@
 package jm.task.core.jdbc;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        String sql = "CREATE TABLE if not exists users " +
+                "(id BIGINT not NULL, " +
+                " name VARCHAR(255), " +
+                " lastName VARCHAR(255), " +
+                " age TINYINT, " +
+                " PRIMARY KEY ( id ))";
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?useSSL=false", "hbstudent", "Fpfptkm1979sql");
+             Statement statement = connection.createStatement()) {
+            System.out.println("Database connection successful!\n");
+            statement.executeUpdate(sql);
+            System.out.println("Table users create successful or already exists!");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
