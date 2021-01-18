9eb2265741160c474d5c63445c86e2aa01eab2ab
ZloyGremlinJE <rellepro@gmail.com>
2021-01-18 16:58:18 +0300

    Add properties

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String CONFIG_NAME = "employee.properties";
+    private static final Properties GLOBAL_CONFIG = new Properties();
+
+    public static void initGlobalConfig() throws IOException {
+        initGlobalConfig(null);
+    }
+
+    public static void initGlobalConfig(String name) throws IOException {
+        if (name != null && !name.trim().isEmpty()) {
+            GLOBAL_CONFIG.load(new FileReader(name));
+        } else {
+            GLOBAL_CONFIG.load(new FileReader(CONFIG_NAME));
+        }
+    }
+
+    public static String getProperty(String property) {
+        return GLOBAL_CONFIG.getProperty(property);
+    }
+
+
 }
